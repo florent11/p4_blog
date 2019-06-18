5329ea31e26324e8c4bc9ff353ad2ad9edebf8c4
florent11 <48155706+florent11@users.noreply.github.com>
2019-06-18 08:13:15 +0200

    Modif chemin d'accès dossier

--- model/commentaires.php
+++ model/commentaires.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'model/model.php';
+require_once 'model.php';
 
 class Commentaire extends Model {
 
@@ -10,4 +10,24 @@ class Commentaire extends Model {
 		AS date_fr, COM_AUTEUR as auteur, COM_CONTENU as contenu, COM_SIGNALER as signaler from comments where BIL_ID=? ORDER BY id ASC';
 		$commentairest = $this->executerRequete($sql, array($idBillet));
 	return $commentairest;
+    }
+
+    public function countComments($idBillet) {
+		$sql = "select count(*) as nbcomments from comments where BIL_ID=?";
+		$commentNumber = $this->executerRequete($sql, array($idBillet));
+	return $commentNumber->fetch();
 	}
+  
+	// Ajouter un commentaire dans la base
+	public function ajouterCommentaire($auteur, $contenu, $idBillet) {
+		$sql = 'insert into comments(COM_DATE, COM_AUTEUR, COM_CONTENU, BIL_ID) values(NOW(), ?, ?, ?)';
+		$this->executerRequete($sql, array($auteur, $contenu, $idBillet));
+	}
+	
+	// Signaler commentaire
+	public function signalerCommentaire($idCommentaire) {
+		$sql = 'UPDATE comments SET COM_SIGNALER = 1 WHERE COM_ID = ?';
+		$this->executerRequete($sql, array($idCommentaire));
+	}
+  
+}
