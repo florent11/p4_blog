83d5911629a81602e0ca9d4a3ca562276770074e
florent11 <48155706+florent11@users.noreply.github.com>
2019-08-01 16:52:33 +0200

    correction faute 'admin' au lieu de 'administration'
    ligne 2

--- view/viewConnexion.php
+++ view/viewConnexion.php
@@ -1,5 +1,5 @@
 <div id="connexionForm">
-	<form class="formulaire" method="POST" action="index.php?action=administration">
+	<form class="formulaire" method="POST" action="index.php?action=admin">
 		<p><label>Pseudonyme<input type="text" name="username"/></label></p>
 		<p><label>Mot de passe<input type="password" name="password"/></label></p>
 		<input type="submit" value="Se connecter"/>
