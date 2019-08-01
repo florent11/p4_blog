9ec588f0bc1503e0c9fe7bb051fe39ead98021f8
florent11 <48155706+florent11@users.noreply.github.com>
2019-08-01 16:55:29 +0200

    Correction syntaxe

--- model/adminRegistration.php
+++ model/adminRegistration.php
@@ -3,11 +3,16 @@
 require_once "model.php";
 
 class adminRegistration extends Model { 
-    //Insertion des identifiants définis par l'Admin
+    //Insertion dans la base de données des identifiants définis par l'Admin
     public function ajoutIdAdmin($name, $pass) {
         $sql = "INSERT INTO members SET id_group=1, name = :nom, pass = :password";
-        $admin= $this->executerRequete($sql, array(":nom"=>$name,":password"=>$pass));   
+        $admin = $this->executerRequete($sql, array(":nom"=>$name,":password"=>$pass));   
     }
+
+    public function verifRegistration() {
+        $sql = "SELECT COUNT(*) AS nb FROM members";
+        $verif = $this->executerRequete($sql);
+        return $verif->fetch();
+    }
+
 }
-
-
