7155cc8860ceaebc35153bcd656f2549b4057d77
florent11 <48155706+florent11@users.noreply.github.com>
2019-08-12 16:51:27 +0200

    Indentation du code

--- view/viewRegistration.php
+++ view/viewRegistration.php
@@ -1,10 +1,10 @@
 <div id="wrapper-home">
-	        <h2>Inscription de l'administrateur du blog</h2>
-    	    <form method="POST" action="index.php?action=adminregistration" id="registration_form">
-		    <p>Nom: <input type="text" name="nom" required /> </p> 
+	<h2>Inscription de l'administrateur du blog</h2>
+    	<form method="POST" action="index.php?action=adminregistration" id="registration_form">
+			<p>Nom: <input type="text" name="nom" required /> </p> 
             <p>Mots de Passe: <input type="password" name="password" required /> </p> <br/>
-    	    </form>
-        <div class="buttonRow">
-	    	<button class="button left" form="registration_form">Envoyer</button>
-		</div>
- </div>
+    	</form>
+    <div class="buttonRow">
+	    <button class="button left" form="registration_form">Envoyer</button>
+	</div>
+</div>
