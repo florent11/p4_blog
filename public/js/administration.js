425795a75ae5e0c6cb3238154ec47ae02f5f49ba
florent11 <48155706+florent11@users.noreply.github.com>
2019-08-26 15:33:57 +0200

    Renommage de "main.js" en "administration.js"; transformation du code en POO et ajout d'une méthode "sortBillet" gérant la couleur des boutons de tri des billets.

---
+++ public/js/administration.js
@@ -0,0 +1,42 @@
+var Administration = {
+	buttonTrash: document.getElementsByClassName("fa-trash"),  // Attribut de sélection du boutton "Trash"
+	buttonRecent: document.getElementById("recent"),  // Attribut de sélection du boutton "Plus Récent"
+	buttonAncien: document.getElementById("ancien"),  // Attribut de sélection du boutton "Plus Ancien"
+	url: window.location.search.substr(1).split("&"),  // Attribut récupérant les infos de l'URL
+	
+	// Méthode qui gère la confirmation de suppression d'un billet
+	deleteAlert: function() {
+		for(var i=0; i < this.buttonTrash.length; i++) {
+			this.buttonTrash[i].addEventListener("click", function(e) {
+			if(confirm("Voulez vous supprimer cet élément ?")){
+				console.log("Billet Supprimé");
+			} 
+			else {
+				e.preventDefault();
+				}
+			});
+		}    
+	},
+
+	// Méthode qui gère la couleur des boutons de tri des billets
+	sortBillet: function() {
+		if(this.url=="action=admin,sort=desc") {
+			this.buttonRecent.style.background = "#6f6f6f";
+			this.buttonRecent.style.color = "white";
+			this.buttonRecent.style.textShadow = "none";
+		}
+		else {
+			this.buttonAncien.style.background = "#6f6f6f";
+			this.buttonAncien.style.color = "white";
+			this.buttonAncien.style.textShadow = "none";	
+		}	
+	},
+
+	// Gestion des évènements 
+	activerEvents: function() {
+		this.deleteAlert();
+		this.sortBillet();
+	}
+};
+
+Administration.activerEvents(); //Appel de la Méthode "Gestion des évènements"			
