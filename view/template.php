e3f92f45962ce2f40dda1421a550e4e8597272fb
florent11 <48155706+florent11@users.noreply.github.com>
2019-08-31 16:26:45 +0200

    ajout de width :"100%" dans script TinyMCE

--- view/template.php
+++ view/template.php
@@ -11,7 +11,7 @@
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" integrity="sha256-39jKbsb/ty7s7+4WzbtELS4vq9udJ+MDjGTD5mtxHZ0=" crossorigin="anonymous" />
 		<!-- Tiny MCE -->
 		<script src="public/tinymce/js/tinymce/tinymce.min.js"></script>
-		<script>tinymce.init({selector:'textarea',
+		<script>tinymce.init({selector:'textarea', width :'100%',
 		entity_encoding : "raw",
 		setup: function (editor) {
 			editor.on('change', function () {
