43911580684e8612ba436564ade07ca15ab57702
florent11 <48155706+florent11@users.noreply.github.com>
2019-09-02 16:39:53 +0200

    Ajout d'un commentaire decrivant la fonction verifRegistration dans adminRegistration.php

--- model/adminRegistration.php
+++ model/adminRegistration.php
@@ -4,13 +4,14 @@ require_once "model.php";
 
 class adminRegistration extends Model 
 { 
-    //Insertion dans la base de données des identifiants définis par l'Admin
+    // Insertion dans la base de données des identifiants définis par l'Admin
     public function ajoutIdAdmin($name, $pass) 
     {
         $sql = "INSERT INTO members SET id_group=1, name = :nom, pass = :password";
         $admin = $this->executerRequete($sql, array(":nom"=>$name,":password"=>$pass));   
     }
-
+    
+    // Verification si une inscription de l'admin est déjà existante dans la base de données
     public function verifRegistration() 
     {
         $sql = "SELECT COUNT(*) AS nb FROM members";
