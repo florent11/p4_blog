1ff68e95ed66e2109eeed5ba3d3a531014f4e7f2
florent11 <48155706+florent11@users.noreply.github.com>
2019-09-29 10:55:24 +0200

    Réorganisation de chaque fonction de la classe controleurBillet

---
+++ controller/controleurBillet.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once 'model/billet.php';
+require_once 'model/commentaires.php';
+require_once 'core/viewClass.php';
+
+class controleurBillet 
+{
+	private $billet;
+	private $commentaire;
+
+	public function __construct() 
+	{
+		$this->billet = new Billet();
+		$this->commentaire = new Commentaire();
+	}
+
+	// Affiche les détails sur un billet
+	public function billet($idBillet) 
+	{
+		$billet = $this->billet->getBillet($idBillet);
+		$commentaires = $this->commentaire->getCommentaires($idBillet);
+		$commentNumber = $this->commentaire->countComments($idBillet);
+		$vue = new View("Billet");
+		$vue->generer(array('billet' => $billet, 'commentaires' => $commentaires, 'totalCommentaires' => $commentNumber));
+	}
+  
+	// Ajoute un commentaire
+	public function commenter($auteur, $contenu, $idBillet) 
+	{
+		$this->commentaire->ajouterCommentaire($auteur, $contenu, $idBillet);
+		$vue = new View("ComPosted");
+		$vue->generer();
+		header ("Refresh: 5;URL=index.php?action=billet&id=" . $idBillet);
+	}
+	
+	// Signaler un commentaire
+	public function signaler($idBillet, $idCommentaire) 
+	{
+		$this->commentaire->signalerCommentaire($idCommentaire);
+		$this->billet($idBillet);
+	}
+}
