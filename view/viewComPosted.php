aab4484e8214030061b9f026826e0c7396cc2c82
florent11 <48155706+florent11@users.noreply.github.com>
2019-09-29 11:40:52 +0200

    création de la vue "viewComPosted"

--- view/viewComPosted.php
+++ view/viewComPosted.php
@@ -3,6 +3,6 @@
 <div id="status">
     <p>Ce commentaire va être soumis à l'administrateur,</p> 
     <p>il sera publié après sa validation.</p>
-    <p>Redirection vers la page du billet dans <span id="compteur"></span>secondes</p>
+    <p>Redirection vers la page du billet dans <span id="compteur"></span> secondes</p>
 </div>
 <script src="public/js/compteur.js"></script>
