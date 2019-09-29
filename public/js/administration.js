766b57812d0ef481fe0101e5e5dfe7f1625855d0
florent11 <48155706+florent11@users.noreply.github.com>
2019-09-29 15:32:36 +0200

    Conversion du code dans un autre style de POO en JavaScript

--- public/js/administration.js
+++ public/js/administration.js
@@ -1,13 +1,17 @@
-var Administration = {
-	buttonTrash: document.getElementsByClassName("fa-trash"),  // Attribut de sélection du bouton "Trash"
-	buttonRecent: document.getElementById("recent"),  // Attribut de sélection du bouton "Plus Récent"
-	buttonAncien: document.getElementById("ancien"),  // Attribut de sélection du bouton "Plus Ancien"
-	urlParams : new URLSearchParams(location.search),  // Attribut récupérant les infos de l'URL
-	logoHome: document.getElementById("logo"),  // Attribut de sélection du logo du blog
+class Administration 
+{
+	constructor() 
+	{
+	this.buttonTrash = document.getElementsByClassName("fa-trash");  // Attribut de sélection du bouton "Trash"
+	this.buttonRecent = document.getElementById("recent");  // Attribut de sélection du bouton "Plus Récent"
+	this.buttonAncien = document.getElementById("ancien");  // Attribut de sélection du bouton "Plus Ancien"
+	this.urlParams = new URLSearchParams(location.search);  // Attribut récupérant les infos de l'URL
+	this.logoHome = document.getElementById("logo");  // Attribut de sélection du logo du blog
+	}
 
-	
 	// Méthode qui gère la confirmation de suppression d'un billet
-	deleteAlert: function() {
+	deleteAlert()
+	{
 		for(var i=0; i < this.buttonTrash.length; i++) {
 			this.buttonTrash[i].addEventListener("click", function(e) {
 			if(!confirm("Voulez vous supprimer cet élément ?")){
@@ -15,10 +19,11 @@ var Administration = {
 				}
 			});
 		}    
-	},
+	}
 
 	// Méthode qui gère la couleur des boutons de tri des billets
-	sortBillet: function() {
+	sortBillet()
+	{
 		if(this.urlParams.has("sort")) {
 			if(this.urlParams.get("sort") == "desc") {
 				this.buttonRecent.style.background = "#6f6f6f";
@@ -31,10 +36,11 @@ var Administration = {
 				this.buttonAncien.style.textShadow = "none";	
 			}
 		}	
-	},
+	}
 
 	// Méthode qui désactive le lien vers la page d'accueil lors du clic sur le logo
-	deleteHomeLink: function(){
+	deleteHomeLink()
+	{
 		if(this.urlParams.has("action")) {
 			if(this.urlParams.get("action") == "create" || this.urlParams.get("action") == "vueModifier" || this.urlParams.get("action") == "billet") {
 				this.logoHome.addEventListener("click", (function(e){
@@ -42,14 +48,16 @@ var Administration = {
 				}));	
 			}	
 		}	
-	},
+	}
 
 	// Gestion des évènements 
-	activerEvents: function() {
-		this.deleteAlert();
-		this.sortBillet();
-		this.deleteHomeLink();
+	activerEvents()
+	{
+		this.deleteAlert();  // Appel de la méthode "deleteAlert"
+		this.sortBillet();  // Appel de la méthode "sortBillet"
+		this.deleteHomeLink();  // Appel de la méthode "deleteHomeLink"
 	}
-};
+}
 
-Administration.activerEvents(); //Appel de la Méthode "Gestion des évènements"			
+const administration = new Administration();
+administration.activerEvents();  // Appel de la méthode "Gestion des évènements"			
