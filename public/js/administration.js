15b18a7dab77fb55809e1146ae8ff9b7e5af5d08
florent11 <48155706+florent11@users.noreply.github.com>
2019-09-29 11:47:34 +0200

    Amélioration d'une méthode récupérant les infos d'une URL; ajout méthode "deleteHomeLink" qui désactive le lien vers la page d'accueil lors du clic sur le logo

--- public/js/administration.js
+++ public/js/administration.js
@@ -1,17 +1,16 @@
 var Administration = {
-	buttonTrash: document.getElementsByClassName("fa-trash"),  // Attribut de sélection du boutton "Trash"
-	buttonRecent: document.getElementById("recent"),  // Attribut de sélection du boutton "Plus Récent"
-	buttonAncien: document.getElementById("ancien"),  // Attribut de sélection du boutton "Plus Ancien"
-	url: window.location.search.substr(1).split("&"),  // Attribut récupérant les infos de l'URL
+	buttonTrash: document.getElementsByClassName("fa-trash"),  // Attribut de sélection du bouton "Trash"
+	buttonRecent: document.getElementById("recent"),  // Attribut de sélection du bouton "Plus Récent"
+	buttonAncien: document.getElementById("ancien"),  // Attribut de sélection du bouton "Plus Ancien"
+	urlParams : new URLSearchParams(location.search),  // Attribut récupérant les infos de l'URL
+	logoHome: document.getElementById("logo"),  // Attribut de sélection du logo du blog
+
 	
 	// Méthode qui gère la confirmation de suppression d'un billet
 	deleteAlert: function() {
 		for(var i=0; i < this.buttonTrash.length; i++) {
 			this.buttonTrash[i].addEventListener("click", function(e) {
-			if(confirm("Voulez vous supprimer cet élément ?")){
-				console.log("Billet Supprimé");
-			} 
-			else {
+			if(!confirm("Voulez vous supprimer cet élément ?")){
 				e.preventDefault();
 				}
 			});
@@ -20,15 +19,28 @@ var Administration = {
 
 	// Méthode qui gère la couleur des boutons de tri des billets
 	sortBillet: function() {
-		if(this.url=="action=admin,sort=desc") {
-			this.buttonRecent.style.background = "#6f6f6f";
-			this.buttonRecent.style.color = "white";
-			this.buttonRecent.style.textShadow = "none";
-		}
-		else if(this.url=="action=admin,sort=asc") {
-			this.buttonAncien.style.background = "#6f6f6f";
-			this.buttonAncien.style.color = "white";
-			this.buttonAncien.style.textShadow = "none";	
+		if(this.urlParams.has("sort")) {
+			if(this.urlParams.get("sort") == "desc") {
+				this.buttonRecent.style.background = "#6f6f6f";
+				this.buttonRecent.style.color = "white";
+				this.buttonRecent.style.textShadow = "none";
+			}
+			else if(this.urlParams.get("sort") == "asc") {
+				this.buttonAncien.style.background = "#6f6f6f";
+				this.buttonAncien.style.color = "white";
+				this.buttonAncien.style.textShadow = "none";	
+			}
+		}	
+	},
+
+	// Méthode qui désactive le lien vers la page d'accueil lors du clic sur le logo
+	deleteHomeLink: function(){
+		if(this.urlParams.has("action")) {
+			if(this.urlParams.get("action") == "create" || this.urlParams.get("action") == "vueModifier" || this.urlParams.get("action") == "billet") {
+				this.logoHome.addEventListener("click", (function(e){
+					e.preventDefault();
+				}));	
+			}	
 		}	
 	},
 
@@ -36,6 +48,7 @@ var Administration = {
 	activerEvents: function() {
 		this.deleteAlert();
 		this.sortBillet();
+		this.deleteHomeLink();
 	}
 };
 
