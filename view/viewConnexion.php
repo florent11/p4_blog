ecd765486a33321db7123f359f8c00613aaaac19
florent11 <48155706+florent11@users.noreply.github.com>
2019-09-29 10:58:44 +0200

    Retrait du "controller" inutile; suppr de l'ancien code "controllerBillet"avec fonctions réorganisées et suppr "viewConnexion" inutile.

--- view/viewConnexion.php
+++
@@ -1,7 +0,0 @@
-<div id="connexionForm">
-	<form class="formulaire" method="POST" action="index.php?action=connexionAdmin">
-		<p><label>Pseudonyme<input type="text" name="username"/></label></p>
-		<p><label>Mot de passe<input type="password" name="password"/></label></p>
-		<input type="submit" value="Se connecter"/>
-	</form>
-</div>
